128ec4471a5f3c149094eca9fa07360a630e6933
Bryan Wyatt <brwyatt@gmail.com>
2018-11-09 10:11:02 -0800

    Implement code to add match to DynamoDB

--- src/F2B/dynamo.py
+++ src/F2B/dynamo.py
@@ -1,12 +1,35 @@
+import logging
+
 import boto3
 from boto3.dynamodb.conditions import Key
 
 
+log = logging.getLogger()
 table = boto3.resource('dynamodb', region_name='us-west-2').Table('F2B')
 
 
-def add_fail(ip, eventid, jail, ttl):
-    return True
+def add_match(host, matchid, filter_name, timestamp, ttl, source='UNKNOWN'):
+    item = {
+        'Host': host,
+        'MatchID': str(matchid),
+        'Filter': filter_name,
+        'Timestamp': timestamp,
+        'Source': source,
+        'TTL': timestamp+ttl
+    }
+    log.debug('Adding Match to DynamoDB: {0}'.format(item))
+    try:
+        resp = table.put_item(Item=item)
+    except Exception as e:
+        log.critical('Exception adding to DynamoDB: {0}: {1}'
+                     .format(e.__class__.__name__, str(e)))
+        return False
+    else:
+        if resp['ResponseMetadata']['HTTPStatusCode'] == 200:
+            return True
+        else:
+            log.critical('Failed to add to DynamoDB: {0}'.format(resp))
+            return False
 
 
 def check_ban(ip, jail):
