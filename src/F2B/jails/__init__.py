07b18226938cf6cb768ace0a925efd7fd43842e3
Bryan Wyatt <brwyatt@gmail.com>
2018-11-10 21:14:04 -0800

    Add jail implementation

---
+++ src/F2B/jails/__init__.py
@@ -0,0 +1,62 @@
+import logging
+from time import time
+
+from F2B.dynamo import get_match_count
+
+
+class Jail():
+    filters = []
+
+    fail_limit = 3
+
+    fail_window = 3600  # 1 hour
+
+    def __init__(self):
+        self.log = logging.getLogger(self.__module__)
+        if self.__class__.__name__ == 'Jail':
+            self.filters = Jail.filters
+        else:
+            self.filters = list(set(Jail.filters + self.__class__.filters))
+
+    def check_bans(self, matches):
+        self.log.info('Checking matches against jail "{0}"'.format(self.name))
+        self.log.debug('Matches: {0}'.format(matches))
+        bans = []
+        hosts = []
+
+        # Get all the matched IPs from the filters we care about
+        for filter_name in self.filters:
+            self.log.info('Collecting matches from filter "{0}"'
+                          .format(filter_name))
+            filter_hosts = [x['Host'] for x in matches.get(filter_name, [])]
+            self.log.debug('Found hosts: {0}'.format(filter_hosts))
+            hosts.extend(filter_hosts)
+
+        # Remove duplicates
+        hosts = list(set(hosts))
+        self.log.debug('Final host list: {0}'.format(hosts))
+
+        if self.fail_window:
+            self.log.debug('fail_window is set, will filter search to window')
+            since = int(time()) - self.fail_window
+        else:
+            self.log.debug('fail_window is no set, not filtering search window')
+            since = None
+
+        for host in hosts:
+            self.log.debug('Checking host "{0}" for filter matches')
+            match_count = get_match_count(host, self.filters, since=since)
+            self.log.debug('Host "{0}" had {1} matches'.format(host,
+                                                               match_count))
+            if match_count >= self.fail_limit:
+                self.log.info('{0} exceeded jail limit and will be banned!')
+                bans.append(host)
+
+        return bans
+
+    @property
+    def name(self):
+        if hasattr(self, 'jail_name'):
+            return self.jail_name.lower()
+        else:
+            return self.__class__.__name__.lower()
---
+++ tests/F2B/test_jails.py
@@ -0,0 +1,72 @@
+import pytest
+
+import F2B.jails
+from F2B.jails import Jail
+
+
+def test_jail():
+    jail = Jail()
+    assert jail.filters == []
+    assert jail.fail_limit == 3
+    assert jail.fail_window == 3600
+    assert jail.name == 'jail'
+
+    class SimpleJail(Jail):
+        filters = ['test']
+        fail_limit = 1
+        fail_window = False
+        jail_name = 'TESTING'
+
+    jail = SimpleJail()
+    assert jail.filters == ['test']
+    assert jail.fail_limit == 1
+    assert jail.fail_window == False
+    assert jail.name == 'testing'
+
+
+@pytest.mark.parametrize(
+    'filters,fail_limit,fail_window,matches,match_counts,result', [
+        # One filter with one host match, with 3 strikes
+        (['filter1'], 3, 3600, {
+            'filter1': [{'Host': '0.0.0.0'}]
+        }, {'0.0.0.0': 3}, ['0.0.0.0']),
+        # One filter with one host match in different filter
+        (['filter2'], 3, 3600, {
+            'filter1': [{'Host': '0.0.0.0'}]
+        }, {}, []),
+        # One filter with one host match, with 2 strikes
+        (['filter1'], 3, 3600, {
+            'filter1': [{'Host': '0.0.0.0'}]
+        }, {'0.0.0.0': 2}, []),
+        # Two filters with the same one host match, with 3 strikes
+        (['filter1', 'filter2'], 3, False, {
+            'filter1': [{'Host': '0.0.0.0'}],
+            'filter2': [{'Host': '0.0.0.0'}],
+        }, {'0.0.0.0': 3}, ['0.0.0.0']),
+        # Two filters with one different host match each, with 3 strikes
+        (['filter1', 'filter2'], 3, False, {
+            'filter1': [{'Host': '0.0.0.0'}],
+            'filter2': [{'Host': '0.0.0.1'}],
+        }, {'0.0.0.0': 3, '0.0.0.1': 3}, ['0.0.0.0', '0.0.0.1']),
+        # Two filters with one different host match each, with 0 and 3 strikes
+        (['filter1', 'filter2'], 3, False, {
+            'filter1': [{'Host': '0.0.0.0'}],
+            'filter2': [{'Host': '0.0.0.1'}],
+        }, {'0.0.0.0': 0, '0.0.0.1': 3}, ['0.0.0.1']),
+    ])
+def test_jail_check_bans(monkeypatch, filters, fail_limit, fail_window, matches,
+                         match_counts, result):
+    jail = Jail()
+    jail.filters = filters
+    jail.fail_limit = fail_limit
+    jail.fail_window = fail_window
+
+    def mock(*args, **kwargs):
+        if match_counts:
+            return match_counts.get(args[0], 0)
+        else:
+            return 0
+
+    monkeypatch.setattr(F2B.jails, 'get_match_count', mock)
+
+    assert sorted(jail.check_bans(matches)) == sorted(result)
