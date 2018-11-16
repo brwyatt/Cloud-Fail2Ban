de35bb904d015e0876af5e3a33d90e792d09e986
Bryan Wyatt <brwyatt@gmail.com>
2018-11-16 10:52:17 -0800

    Add tests for jail processing

--- tests/test_F2B.py
+++ tests/test_F2B.py
@@ -1,14 +1,23 @@
 import pytest
 from uuid import uuid5
 
-from F2B import process_log_events
+from F2B import process_log_events, process_jails
 from F2B.filters import Filter
+from F2B.jails import Jail
 
 
 class SimpleTestFilter(Filter):
     failregexes = [
         'This Host Matches: %(host)s'
     ]
+
+
+class SimpleTestJail(Jail):
+    filters = [
+        'tests.test_F2B.SimpleTestFilter',
+    ]
+
+    fail_limit = 0
 
 
 @pytest.mark.parametrize("test_data,filters,result", [
@@ -56,3 +65,53 @@ class SimpleTestFilter(Filter):
 ])
 def test_process_log_events(test_data, filters, result):
     assert sorted(process_log_events(test_data, filters)) == sorted(result)
+
+
+@pytest.mark.parametrize("test_data,jails,result", [
+    ({},
+     [Jail],
+     {}),
+    ({
+        'tests.test_F2B.SimpleTestFilter': [],
+     },
+     [Jail],
+     {}),
+    ({
+        'tests.test_F2B.SimpleTestFilter': [],
+        'F2B.filters.Filter': []
+     },
+     [Jail, SimpleTestJail],
+     {}),
+    ({
+        'F2B.filters.Filter': [
+            {
+                'Host': '127.0.0.1',
+                'MatchID': uuid5(Filter().uuid, '21245'),
+                'Timestamp': 1541698946
+            },
+            {
+                'Host': '127.0.0.2',
+                'MatchID': uuid5(Filter().uuid, '23345'),
+                'Timestamp': 1541698946
+            }
+        ],
+        'tests.test_F2B.SimpleTestFilter': [
+            {
+                'Host': '127.0.1.1',
+                'MatchID': uuid5(SimpleTestFilter().uuid, '21245'),
+                'Timestamp': 1541698946
+            },
+            {
+                'Host': '127.0.1.2',
+                'MatchID': uuid5(SimpleTestFilter().uuid, '23345'),
+                'Timestamp': 1541698946
+            }
+        ]
+     },
+     [SimpleTestJail],
+     {
+         'simpletestjail': ['127.0.1.1', '127.0.1.2']
+     }),
+])
+def test_process_jails(test_data, jails, result):
+    assert sorted(process_jails(test_data, jails)) == sorted(result)
