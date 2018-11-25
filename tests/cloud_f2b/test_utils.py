c2034be7ce6040ed2a6c2c32c4c2efa0dd5a5c37
Bryan Wyatt <brwyatt@gmail.com>
2018-11-25 12:18:04 -0800

    Add more test cases for dynamodb_event_to_matches
    Should probably test that multiple INSERTs actually end up in the result
    correctly.

--- tests/cloud_f2b/test_utils.py
+++ tests/cloud_f2b/test_utils.py
@@ -97,6 +97,234 @@ def test_decompress_cloudwatch_event(test_data):
             ]
         }
     ),
+    (  # Test two INSERTs of different filters
+        {
+            'Records': [
+                {
+                    'eventID': 'g247h8ohgiugsdfh',
+                    'eventName': 'INSERT',
+                    'eventVersion': '1.1',
+                    'eventSource': 'aws:dynamodb',
+                    'awsRegion': 'us-test-1',
+                    'dynamodb': {
+                        'ApproximateCreationDateTime': 1543169340,
+                        'Keys': {
+                            'MatchID': {
+                                'S': '6fb63653-eee5-50d9-97fa-0a75545ab61e'
+                            },
+                            'Host': {
+                                'S': '127.0.0.1'
+                            }
+                        },
+                        'NewImage': {
+                            'Filter': {
+                                'S': 'cloud_f2b.filters.auth.sshd.Sshd'
+                            },
+                            'MatchID': {
+                                'S': '6fb63653-eee5-50d9-97fa-0a75545ab61e'
+                            },
+                            'EventID': {
+                                'S': '4646816316581646456'
+                            },
+                            'Host': {
+                                'S': '127.0.0.1'
+                            },
+                            'TTL': {
+                                'N': '1543255747'
+                            },
+                            'Timestamp': {
+                                'N': '1543169347'
+                            },
+                            'Source': {
+                                'S': 'some_host'
+                            }
+                        },
+                        'SequenceNumber': '11111111111',
+                        'SizeBytes': 11,
+                        'StreamViewType': 'NEW_AND_OLD_IMAGES'
+                    },
+                    'eventSourceARN': 'arn:aws:dynamodb:us-test-1:1111:'
+                                      'table/F2B/stream/2011-11-11'
+                },
+                {
+                    'eventID': 'hg9wh90hgiusodf',
+                    'eventName': 'INSERT',
+                    'eventVersion': '1.1',
+                    'eventSource': 'aws:dynamodb',
+                    'awsRegion': 'us-test-1',
+                    'dynamodb': {
+                        'ApproximateCreationDateTime': 1543171641,
+                        'Keys': {
+                            'MatchID': {
+                                'S': '5c137d7e-5923-477a-b0ba-ca4355185b50'
+                            },
+                            'Host': {
+                                'S': '127.0.1.1'
+                            }
+                        },
+                        'NewImage': {
+                            'Filter': {
+                                'S':
+                                'cloud_f2b.filters.auth.sshd-ddos.Sshd-Ddos'
+                            },
+                            'MatchID': {
+                                'S': '5c137d7e-5923-477a-b0ba-ca4355185b50'
+                            },
+                            'EventID': {
+                                'S': '98342987592376'
+                            },
+                            'Host': {
+                                'S': '127.0.1.1'
+                            },
+                            'TTL': {
+                                'N': '1543256812'
+                            },
+                            'Timestamp': {
+                                'N': '1543170412'
+                            },
+                            'Source': {
+                                'S': 'some_host'
+                            }
+                        },
+                        'SequenceNumber': '222222222',
+                        'SizeBytes': 11,
+                        'StreamViewType': 'NEW_AND_OLD_IMAGES'
+                    },
+                    'eventSourceARN': 'arn:aws:dynamodb:us-test-1:1111:'
+                                      'table/F2B/stream/2011-11-11'
+                }
+            ]
+        },
+        {
+            'cloud_f2b.filters.auth.sshd.Sshd': [
+                {
+                    'Host': '127.0.0.1',
+                    'MatchID': '6fb63653-eee5-50d9-97fa-0a75545ab61e',
+                    'Timestamp': 1543169347
+                }
+            ],
+            'cloud_f2b.filters.auth.sshd-ddos.Sshd-Ddos': [
+                {
+                    'Host': '127.0.1.1',
+                    'MatchID': '5c137d7e-5923-477a-b0ba-ca4355185b50',
+                    'Timestamp': 1543170412
+                }
+            ]
+        }
+    ),
+    (  # Test two INSERTs of the same filter
+        {
+            'Records': [
+                {
+                    'eventID': 'g247h8ohgiugsdfh',
+                    'eventName': 'INSERT',
+                    'eventVersion': '1.1',
+                    'eventSource': 'aws:dynamodb',
+                    'awsRegion': 'us-test-1',
+                    'dynamodb': {
+                        'ApproximateCreationDateTime': 1543169340,
+                        'Keys': {
+                            'MatchID': {
+                                'S': '6fb63653-eee5-50d9-97fa-0a75545ab61e'
+                            },
+                            'Host': {
+                                'S': '127.0.0.1'
+                            }
+                        },
+                        'NewImage': {
+                            'Filter': {
+                                'S': 'cloud_f2b.filters.auth.sshd.Sshd'
+                            },
+                            'MatchID': {
+                                'S': '6fb63653-eee5-50d9-97fa-0a75545ab61e'
+                            },
+                            'EventID': {
+                                'S': '4646816316581646456'
+                            },
+                            'Host': {
+                                'S': '127.0.0.1'
+                            },
+                            'TTL': {
+                                'N': '1543255747'
+                            },
+                            'Timestamp': {
+                                'N': '1543169347'
+                            },
+                            'Source': {
+                                'S': 'some_host'
+                            }
+                        },
+                        'SequenceNumber': '11111111111',
+                        'SizeBytes': 11,
+                        'StreamViewType': 'NEW_AND_OLD_IMAGES'
+                    },
+                    'eventSourceARN': 'arn:aws:dynamodb:us-test-1:1111:'
+                                      'table/F2B/stream/2011-11-11'
+                },
+                {
+                    'eventID': 'hg9wh90hgiusodf',
+                    'eventName': 'INSERT',
+                    'eventVersion': '1.1',
+                    'eventSource': 'aws:dynamodb',
+                    'awsRegion': 'us-test-1',
+                    'dynamodb': {
+                        'ApproximateCreationDateTime': 1543171641,
+                        'Keys': {
+                            'MatchID': {
+                                'S': '5c137d7e-5923-477a-b0ba-ca4355185b50'
+                            },
+                            'Host': {
+                                'S': '127.0.1.1'
+                            }
+                        },
+                        'NewImage': {
+                            'Filter': {
+                                'S':
+                                'cloud_f2b.filters.auth.sshd.Sshd'
+                            },
+                            'MatchID': {
+                                'S': '5c137d7e-5923-477a-b0ba-ca4355185b50'
+                            },
+                            'EventID': {
+                                'S': '98342987592376'
+                            },
+                            'Host': {
+                                'S': '127.0.1.1'
+                            },
+                            'TTL': {
+                                'N': '1543256812'
+                            },
+                            'Timestamp': {
+                                'N': '1543170412'
+                            },
+                            'Source': {
+                                'S': 'some_host'
+                            }
+                        },
+                        'SequenceNumber': '222222222',
+                        'SizeBytes': 11,
+                        'StreamViewType': 'NEW_AND_OLD_IMAGES'
+                    },
+                    'eventSourceARN': 'arn:aws:dynamodb:us-test-1:1111:'
+                                      'table/F2B/stream/2011-11-11'
+                }
+            ]
+        },
+        {
+            'cloud_f2b.filters.auth.sshd.Sshd': [
+                {
+                    'Host': '127.0.0.1',
+                    'MatchID': '6fb63653-eee5-50d9-97fa-0a75545ab61e',
+                    'Timestamp': 1543169347
+                },
+                {
+                    'Host': '127.0.1.1',
+                    'MatchID': '5c137d7e-5923-477a-b0ba-ca4355185b50',
+                    'Timestamp': 1543170412
+                }
+            ]
+        }
+    ),
     (  # Test that we ignore REMOVE events
         {
             'Records': [
@@ -152,4 +380,4 @@ def test_decompress_cloudwatch_event(test_data):
     )
 ])
 def test_dynamodb_event_to_matches(test_data, result):
-    assert utils.dynamodb_event_to_matches(test_data) == result
+    assert sorted(utils.dynamodb_event_to_matches(test_data)) == sorted(result)
