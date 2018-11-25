badb387641698d6d693e0854d18277ace69fa4b5
Bryan Wyatt <brwyatt@gmail.com>
2018-11-25 12:24:16 -0800

    Add test case for MODIFY
    We should process MODIFYs the same as INSERTs, except there's the extra
    OldImage to be ignored.

--- tests/cloud_f2b/test_utils.py
+++ tests/cloud_f2b/test_utils.py
@@ -97,6 +97,90 @@ def test_decompress_cloudwatch_event(test_data):
             ]
         }
     ),
+    (  # Test single MODIFY
+        {
+            'Records': [
+                {
+                    'eventID': 'g247h8ohgiugsdfh',
+                    'eventName': 'MODIFY',
+                    'eventVersion': '1.1',
+                    'eventSource': 'aws:dynamodb',
+                    'awsRegion': 'us-test-1',
+                    'dynamodb': {
+                        'ApproximateCreationDateTime': 1543169340,
+                        'Keys': {
+                            'MatchID': {
+                                'S': '6fb63653-eee5-50d9-97fa-0a75545ab61e'
+                            },
+                            'Host': {
+                                'S': '127.0.0.1'
+                            }
+                        },
+                        'NewImage': {
+                            'Filter': {
+                                'S': 'cloud_f2b.filters.auth.sshd.Sshd'
+                            },
+                            'MatchID': {
+                                'S': '6fb63653-eee5-50d9-97fa-0a75545ab61e'
+                            },
+                            'EventID': {
+                                'S': '4646816316581646456'
+                            },
+                            'Host': {
+                                'S': '127.0.0.1'
+                            },
+                            'TTL': {
+                                'N': '1543255747'
+                            },
+                            'Timestamp': {
+                                'N': '1543169347'
+                            },
+                            'Source': {
+                                'S': 'some_host'
+                            }
+                        },
+                        'OldImage': {
+                            'Filter': {
+                                'S': 'cloud_f2b.filters.auth.sshd.Sshd'
+                            },
+                            'MatchID': {
+                                'S': '6fb63653-eee5-50d9-97fa-0a75545ab61e'
+                            },
+                            'EventID': {
+                                'S': '59658246596326'
+                            },
+                            'Host': {
+                                'S': '127.0.0.1'
+                            },
+                            'TTL': {
+                                'N': '1543244636'
+                            },
+                            'Timestamp': {
+                                'N': '1543158236'
+                            },
+                            'Source': {
+                                'S': 'some_previous_host'
+                            }
+                        },
+                        'SequenceNumber': '11111111111',
+                        'SizeBytes': 11,
+                        'StreamViewType': 'NEW_AND_OLD_IMAGES'
+                    },
+                    'eventSourceARN': 'arn:aws:dynamodb:us-test-1:1111:'
+                                      'table/F2B/stream/2011-11-11'
+                }
+            ]
+        },
+        {
+            'cloud_f2b.filters.auth.sshd.Sshd': [
+                {
+                    'Host': '127.0.0.1',
+                    'MatchID': '6fb63653-eee5-50d9-97fa-0a75545ab61e',
+                    'Timestamp': 1543169347
+                }
+            ]
+        }
+    ),
     (  # Test two INSERTs of different filters
         {
             'Records': [
